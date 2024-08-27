0b12bad4c29b2dd6c2c8cfbcbf3853d0f2f6402b
LucyC-1225 <lucyc1225xx@gmail.com>
2024-08-26 21:19:40 -0400

    References #2 - made style property work

--- index.jsx
+++ index.jsx
@@ -5,11 +5,24 @@ import Quote from "./Quote"
 function App() {
   const [quoteText, setQuoteText] = React.useState("");
   const [quoteAuthor, setQuoteAuthor] = React.useState("");
+  /* style the background color, text color, a background color and button background color.*/
+  const [backgroundStyle, setBackgroundStyle] = React.useState({});
 
-  /* fetch a quote when page loads */
+  /* fetch a quote when page loads, also generate a random color*/
   React.useEffect(() => {
     fetchNewQuote();
+    setBackgroundStyle(generateColorStyle());
   }, []);
+
+  /* generates random color from array of colors*/
+  function generateColorStyle() {
+    const styles = {
+      backgroundColor: '#c1d3fe',
+      color: '#c1d3fe'
+    }
+    console.log(styles);
+    return styles;
+  }
 
   /* fetches new quote from API and sets the appropriate states */
   function fetchNewQuote() {
@@ -20,7 +33,7 @@ function App() {
 
 
   return (
-    <div>
+    <div id = "quote-container" style = {backgroundStyle}>
       <Quote 
       quoteText = {quoteText}
       quoteAuthor = {quoteAuthor}
