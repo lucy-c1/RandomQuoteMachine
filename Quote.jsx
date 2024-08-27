84f640d6ab8b9e980541909f0d228362e3e76502
LucyC-1225 <lucyc1225xx@gmail.com>
2024-08-26 22:51:45 -0400

    Resolves #2 - added styles

--- Quote.jsx
+++ Quote.jsx
@@ -1,21 +1,33 @@
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
+import { faTumblr, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 export default function Quote(props) {
     return (
         <div id="quote-box">
-            <h1 id = "text">{props.quoteText}</h1>
-            <h2 id = "author">{props.quoteAuthor}</h2>
+            <div className = "quotetext-container">
+                <FontAwesomeIcon icon={faQuoteLeft} size = "2x"/>
+                <span id = "text">{props.quoteText}</span>
+            </div>
+            <div className = "author-container">
+                <h2 id = "author">- {props.quoteAuthor}</h2>
+            </div>
             {/* https://twitter.com/intent/tweet?text=[TEXT]&url=[URL]&hashtags=[HASHTAGS]
             https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22Certain%20things%20catch%20your%20eye%2C%20but%20pursue%20only%20those%20that%20capture%20the%20heart.%22%20%20Ancient%20Indian%20Proverb
             https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=%22There%20are%20no%20traffic%20jams%20along%20the%20extra%20mile.%22%20Roger%20Staubach
             */}
-            <a href="https://twitter.com/intent/tweet" target="_blank" className = "style-a" id="tweet-quote">twitter</a>
             {/* tumrbl intent url: https://www.tumblr.com/widgets/share/tool?canonicalUrl=[URL]&title=[TITLE]&caption=[CAPTION] 
             https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=%20Ancient%20Indian%20Proverb&content=Certain%20things%20catch%20your%20eye%2C%20but%20pursue%20only%20those%20that%20capture%20the%20heart.&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button
             https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=Roger%20Staubach&content=There%20are%20no%20traffic%20jams%20along%20the%20extra%20mile.&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button
             */}
-            <a href="https://www.tumblr.com/widgets/share/" target="_blank" className = "style-a" id = "tumblr-quote">tumblr</a>
-            <button id="new-quote" onClick = {props.fetchNewQuote}>New Quote</button>
+            <div className = "bottom-container">
+                <div className = "icons-container">
+                    <a href="https://twitter.com/intent/tweet" target="_blank" className = "style-a" id="tweet-quote" style = {props.backgroundStyle}><FontAwesomeIcon icon={faTwitter} size = "lg" className = "icon" /></a>
+                    <a href="https://www.tumblr.com/widgets/share/" target="_blank" className = "style-a" id = "tumblr-quote" style = {props.backgroundStyle}><FontAwesomeIcon icon={faTumblr} size = "lg" className = "icon" /></a>
+                </div>
+                <button id="new-quote" onClick = {props.fetchNewQuote} style = {props.backgroundStyle}>New Quote</button>
+            </div>
         </div>
     )
 }
--- index.jsx
+++ index.jsx
@@ -6,38 +6,59 @@ function App() {
   const [quoteText, setQuoteText] = React.useState("");
   const [quoteAuthor, setQuoteAuthor] = React.useState("");
   /* style the background color, text color, a background color and button background color.*/
-  const [backgroundStyle, setBackgroundStyle] = React.useState({});
+  const [styles, setStyles] = React.useState({});
 
   /* fetch a quote when page loads, also generate a random color*/
   React.useEffect(() => {
     fetchNewQuote();
-    setBackgroundStyle(generateColorStyle());
+    setStyles(generateColorStyle());
   }, []);
 
   /* generates random color from array of colors*/
   function generateColorStyle() {
-    const styles = {
+    // first one for the quote-container div, the other ones for the button and icons in <Quote /> component
+    const styles = [{
       backgroundColor: '#c1d3fe',
       color: '#c1d3fe'
-    }
+    },
+    {
+      backgroundColor: '#c1d3fe',
+    }]
     console.log(styles);
     return styles;
   }
 
-  /* fetches new quote from API and sets the appropriate states */
+  /* fetches new quote from API and sets the appropriate states 
+  https://api.quotable.io/random
+  format:
+  {
+  _id: string
+  // The quotation text
+  content: string
+  // The full name of the author
+  author: string
+  // The `slug` of the quote author
+  authorSlug: string
+  // The length of quote (number of characters)
+  length: number
+  // An array of tag names for this quote
+  tags: string[]
+}
+  */
   function fetchNewQuote() {
     console.log("fetchNewQuote function called");
-    setQuoteText("Quote text test");
+    setQuoteText("I didn’t fail the test. I just found 100 ways to do it wrong.");
     setQuoteAuthor("Quote author test");
   }
 
 
   return (
-    <div id = "quote-container" style = {backgroundStyle}>
+    <div id = "quote-container" style = {styles[0]}>
       <Quote 
       quoteText = {quoteText}
       quoteAuthor = {quoteAuthor}
       fetchNewQuote = {fetchNewQuote}
+      backgroundStyle = {styles[1]}
       />
     </div>
   )
